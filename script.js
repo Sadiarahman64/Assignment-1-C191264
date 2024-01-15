767ff0bb096ce715a887084dd4975013eea89644
Sadiarahman64 <sadiarahman.naima@gmail.com>
2024-01-16 01:20:06 +0600

    changes done

--- script.js
+++ script.js
@@ -12,71 +12,95 @@ var requestOptions = {
     method: 'GET',
     redirect: 'follow'
 };
-  
+
 fetch("http://localhost:3000/budget", requestOptions)
     .then(response => response.text())
     .then(result => console.log(result))
     .catch(error => console.log('error', error));
 
 function formatMoney(value) {
-  return Math.abs(Number(value)).toLocaleString(undefined, {
-    minimumFractionDigits: 2,
-  });
+    return Math.abs(Number(value)).toLocaleString(undefined, {
+        minimumFractionDigits: 2,
+    });
 }
 
 function calculateIncome() {
-  let sum = 0;
-  for (let item of incomeList.children) {
-    const valueString =
-      item.children[0].children[1].children[0].innerHTML.replace(/,/g, "");
+    let sum = 0;
+    for (let item of incomeList.children) {
+        const valueString =
+            item.children[0].children[1].children[0].innerHTML.replace(/,/g, "");
 
-    console.log(parseFloat(valueString));
-    sum += parseFloat(valueString);
-  }
-  totalIncome.innerHTML = formatMoney(sum);
+        console.log(parseFloat(valueString));
+        sum += parseFloat(valueString);
+    }
+    totalIncome.innerHTML = formatMoney(sum);
 }
 calculateIncome();
 
-/**
- * Task 1: Calculate total expense
- */
-function calculateExpense() {}
+
+function calculateExpense() {
+    let sum = 0;
+    for (let item of expenseList.children) {
+        const valueString =
+            item.children[0].children[1].children[0].innerHTML.replace(/,/g, "");
+
+        console.log(parseFloat(valueString));
+        sum += parseFloat(valueString);
+    }
+    totalExpense.innerHTML = formatMoney(sum);
+}
+calculateExpense();
 
 /**
  * Task 2: Calculate the budget
  */
 
-function calculateBudget() {}
+function calculateBudget() {
+    let income = parseFloat(totalIncome.innerHTML.replace(/,/g, ""));
+    let expense = parseFloat(totalExpense.innerHTML.replace(/,/g, ""));
+    let budget = income - expense;
+    totalBudget.innerHTML = formatMoney(budget);
+}
+calculateBudget();
 
 /**
  * Task 3: Delete Entry
  */
-function deleteEntry() {}
+function deleteEntry() {
+    console.log(entryMoney);
+    const entryItem = Event.target.closest("li");
+
+    if (entryItem) {
+        entryItem.parentNode.removeChild(entryItem);
+        alert("Deleted Successfully");
+
+    }
+}
 
 function addEntry() {
-  const type = selectInput.value;
-  const description = descriptionInput.value;
-  const value = valueInput.value;
+    const type = selectInput.value;
+    const description = descriptionInput.value;
+    const value = valueInput.value;
 
-  // data validation
-  const errors = [];
-  if (description.length === 0) {
-    errors.push("Please enter the description");
-  }
-  if (value.length === 0) {
-    errors.push("Please enter the value");
-  }
-  if (errors.length > 0) {
-    alert(errors);
-    return;
-  }
+    // data validation
+    const errors = [];
+    if (description.length === 0) {
+        errors.push("Please enter the description");
+    }
+    if (value.length === 0) {
+        errors.push("Please enter the value");
+    }
+    if (errors.length > 0) {
+        alert(errors);
+        return;
+    }
 
-  // insert entry
-  const list = type === "income" ? incomeList : expenseList;
-  const sign = type === "income" ? "+" : "-";
-  const colorClass = type === "income" ? "text-green-600" : "text-red-600";
+    // insert entry
+    const list = type === "income" ? incomeList : expenseList;
+    const sign = type === "income" ? "+" : "-";
+    const colorClass = type === "income" ? "text-green-600" : "text-red-600";
 
-  const newEntryHtml = `
+    const newEntryHtml = `
     <li class="py-2.5">
       <div class="group flex justify-between gap-2 text-sm">
         <span>${description}</span>
@@ -92,11 +116,13 @@ function addEntry() {
     </li>
     `;
 
-  // Approach 1:
-  list.innerHTML += newEntryHtml;
+    // Approach 1:
+    list.innerHTML += newEntryHtml;
 
-  // update total income value
-  calculateIncome();
+    // update total income value
+    calculateIncome();
+    calculateExpense();
+    calculateBudget();
 }
 
 addExpenseButton.addEventListener("click", addEntry);
